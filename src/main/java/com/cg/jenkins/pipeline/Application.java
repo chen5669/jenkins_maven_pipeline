04ea05d842004c8b59912a158f6582c8d07471ef
chen5669 <cgsnd5669@163.com>
2020-10-06 00:34:49 +0800

    Update Application.java

--- src/main/java/com/cg/jenkins/pipeline/Application.java
+++ src/main/java/com/cg/jenkins/pipeline/Application.java
@@ -3,5 +3,6 @@ package com.cg.jenkins.pipeline;
 public class Application {
     public static void main(String[] args) {
         System.out.println("hello jenkins pipeline!!!!!!");
+        System.out.println("hello jenkins pipeline!!!!!!1111111");
     }
 }
